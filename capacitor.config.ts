649cc197c41b8a22221e79ad8472a6053c0e45f4
Mariano Sebastian Oviedo <pasandolaleca@gmail.com>
2024-10-30 16:40:11 -0300

    splash screen & icons added

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -9,6 +9,22 @@ const config: CapacitorConfig = {
   cleartext:true
   },
   plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      launchFadeOutDuration: 3000,
+      backgroundColor: "#ffffffff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: true,
+    },
     CapacitorHttp:{
       enabled: true
     }
